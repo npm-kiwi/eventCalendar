e70f40b53c5840db51c2fd69374875e8da5be63c
Martin Zaninovich <martinzaninovich5@gmail.com>
2022-09-15 18:28:55 -0300

    task 2

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+// Task 2
+
+function addEvent() {
+    if(eventName.value === "" || eventDate.value === "") {
+        return
+    }
+    if(dateDiff(eventDate.value) < 0) {
+        return
+    }
+    const newEvent = {
+        id: (Math.random() * 100).toString(36).slice(3),
+        name: eventName.value,
+        date: eventDate.value
+    }
+}
 
