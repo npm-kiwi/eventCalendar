b52038e138d5ac11af2d7a6d4951e299bd09e1eb
Martin Zaninovich <martinzaninovich5@gmail.com>
2022-09-15 18:30:28 -0300

    task 1

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+// Task 1
+let events = [];
+
+document.querySelector("#eventName");
+document.querySelector("#eventDate");
+document.querySelector("#bAdd");
+
+document.querySelector("#tasksContainer");
+
+document.querySelector("form").addEventListener("submit", (e) => e.preventDefault);
+
+buttonAdd.addEventListener("keyup", (e) => {
+    if(e.key === "Enter") {
+        e.preventDefault;
+        addEvent();
+    }
+})
+
 
