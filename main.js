00a52050c23af24bac8509cd1ea4f5e7afaae499
Martin Zaninovich <martinzaninovich5@gmail.com>
2022-09-15 18:52:12 -0300

    tarea 1_2

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 // Task 1
 let events = [];
 
-document.querySelector("#eventName");
-document.querySelector("#eventDate");
-document.querySelector("#bAdd");
+const eventName = document.querySelector("#eventName");
+const eventDate = document.querySelector("#eventDate");
+const bAdd = document.querySelector("#bAdd");
 
-document.querySelector("#tasksContainer");
+document.querySelector("#tasksContainer").innerHTML = eventsHTML.join("");
 
 document.querySelector("form").addEventListener("submit", (e) => e.preventDefault);
 
-buttonAdd.addEventListener("keyup", (e) => {
-    if(e.key === "Enter") {
+buttonAdd.addEventListener("click", (e) => {
         e.preventDefault;
         addEvent();
-    }
 })
 
 
